aac905cb8d115d23cdf5bb0f62a14c2a06c9a8e9
kubala1210 <jakubgromala@wp.pl>
2025-08-27 17:51:25 +0100

    add connection string

--- database.py
+++ database.py
@@ -6,16 +6,15 @@ Base = declarative_base()
 
 # podmien na inne parametry, jesli potrzebujesz
 # lub ustaw zmienne srodowiskowe
-DB_USER = "myuser"
-DB_PASSWORD = "mypassword"
-DB_HOST = "localhost"
-DB_PORT = "5435"
-DB_NAME = "shop"
+# DB_USER = "myuser"
+# DB_PASSWORD = "mypassword"
+# DB_HOST = "localhost"
+# DB_PORT = "5435"
+# DB_NAME = "shop"
 
-DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = "postgresql+psycopg2://postgres:grosales@localhost:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 # echo=True - włącza logowanie zapytań SQL do konsoli
 # engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine)
-
